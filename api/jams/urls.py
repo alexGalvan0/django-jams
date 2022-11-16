f5da247d6c839f7a2f7e1ebb1f2aaf152c69b83a
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-16 16:36:23 +0000

    updated views

--- api/jams/urls.py
+++ api/jams/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'songs', views.SongViewSet)
-router.register(r'albums', views.SongViewSet)
-router.register(r'artists', views.SongViewSet)
-router.register(r'genres', views.SongViewSet)
-router.register(r'playlists', views.SongViewSet)
+router.register(r'albums', views.AlbumViewSet)
+router.register(r'artists', views.ArtistViewSet)
+router.register(r'genres', views.GenreViewSet)
+router.register(r'playlists', views.PlaylistViewSet)
 
 
 # Wire up our API using automatic URL routing.
--- api/jams/views.py
+++ api/jams/views.py
@@ -4,25 +4,32 @@ from rest_framework import permissions
 from rest_framework import viewsets
 # Create your views here.
 
+
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
     permission_classes = [permissions.AllowAny]
+
+
 class AlbumViewSet(viewsets.ModelViewSet):
-    queryset = Song.objects.all()
+    queryset = Album.objects.all()
     serializer_class = AlbumSerializer
     permission_classes = [permissions.AllowAny]
+
+
 class ArtistViewSet(viewsets.ModelViewSet):
-    queryset = Song.objects.all()
+    queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
     permission_classes = [permissions.AllowAny]
+
+
 class GenreViewSet(viewsets.ModelViewSet):
-    queryset = Song.objects.all()
+    queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = [permissions.AllowAny]
+
+
 class PlaylistViewSet(viewsets.ModelViewSet):
-    queryset = Song.objects.all()
+    queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
     permission_classes = [permissions.AllowAny]
-
-    
