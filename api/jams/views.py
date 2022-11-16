9ed2e2afec0a102a5fb9f8c52521bbd0f041b092
alexGalvan0 <alex.galvan@student.awesomeinc.org>
2022-11-16 18:41:47 +0000

    updated album model

--- api/jams/views.py
+++ api/jams/views.py
@@ -8,28 +8,28 @@ from rest_framework import viewsets
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
-    permission_classes = [permissions.AllowAny]
+
 
 
 class AlbumViewSet(viewsets.ModelViewSet):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
-    permission_classes = [permissions.AllowAny]
+
 
 
 class ArtistViewSet(viewsets.ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistSerializer
-    permission_classes = [permissions.AllowAny]
+
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [permissions.AllowAny]
+
 
 
 class PlaylistViewSet(viewsets.ModelViewSet):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
-    permission_classes = [permissions.AllowAny]
+
